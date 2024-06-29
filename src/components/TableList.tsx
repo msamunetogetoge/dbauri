7f2b8e8f04f22db4ac4d91a5b636bfd0cd251a71
kenji_tobishima <kenjiii534@gmail.com>
2024-06-29 17:58:37 +0900

    スキーマ、テーブル定義を確認できる機能作成

--- src/components/TableList.tsx
+++ src/components/TableList.tsx
@@ -1,12 +1,71 @@
-import { Component } from "solid-js";
+import {
+  Component,
+  createEffect,
+  createSignal,
+  onCleanup,
+  onMount,
+} from "solid-js";
+import { invoke } from "@tauri-apps/api";
+import { Button, NavLink, Stack } from "solid-bootstrap";
+import TableDetails from "./TableDetails";
 
-const TableList: Component = () => {
-  // Implement table list fetching logic here
+interface TableListProps {
+  schema: string;
+}
+
+const TableList: Component<TableListProps> = (props) => {
+  const [tables, setTables] = createSignal<string[]>([]);
+  const [selectedTable, setSelectedTable] = createSignal<string | null>(null);
+
+  const [loading, setLoading] = createSignal<boolean>(false);
+  const [error, setError] = createSignal<string | null>(null);
+
+  const fetchTables = async () => {
+    setLoading(true);
+    try {
+      const response: string[] = await invoke("get_tables", {
+        schema: props.schema,
+      });
+      setTables(response);
+      setError(null);
+    } catch (err) {
+      setError("Failed to fetch tables.");
+      console.error("Error fetching tables:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  createEffect(() => {
+    fetchTables();
+  });
 
   return (
-    <div class="sidebar">
-      <h4>Tables</h4>
-      <ul>{/* Implement table list rendering */}</ul>
+    <div class="table-list" style={{ "max-height": "100%" }}>
+      <h4 style={{ "overflow-wrap": "break-word" }}>
+        Tables in {props.schema}
+      </h4>
+      {loading() && <p>Loading tables...</p>}
+      {error() && <p class="error">{error()}</p>}
+      <Stack style={{ "max-height": "55rem", "overflow-y": "auto" }}>
+        {tables().map((table) => (
+          <Button
+            variant="secondary"
+            class="px-1 my-1"
+            style={{ "overflow-wrap": "break-word", "text-align": "left" }}
+            onClick={() => setSelectedTable(table)}
+          >
+            {table}
+          </Button>
+        ))}
+      </Stack>
+      {selectedTable() && (
+        <TableDetails
+          schema={props.schema}
+          table={selectedTable()!}
+          onClose={() => setSelectedTable(null)}
+        />
+      )}
     </div>
   );
 };
