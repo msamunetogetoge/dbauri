3485048297bd6101c8377af2153eef918949e6e3
kenji_tobishima <kenjiii534@gmail.com>
2024-07-25 13:26:41 +0900

    watch追加

--- farm.config.ts
+++ farm.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
         bootstrap: path.resolve(__dirname, "node_modules/bootstrap"),
       },
     },
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
   },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
@@ -22,9 +26,5 @@ export default defineConfig({
   server: {
     port: 1420,
     strictPort: true,
-    // watch: {
-    //   // 3. tell vite to ignore watching `src-tauri`
-    //   ignored: ["**/src-tauri/**"],
-    // },
   },
 });
